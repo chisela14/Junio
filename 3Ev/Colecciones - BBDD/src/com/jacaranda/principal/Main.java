879a576a4c740428e416703cacdb7a80fd1777ab
chisela14 <chisela14@gmail.com>
2022-06-18 18:43:58 +0200

    cambio principal

--- 3Ev/Colecciones - BBDD/src/com/jacaranda/principal/Main.java
+++ 3Ev/Colecciones - BBDD/src/com/jacaranda/principal/Main.java
@@ -9,28 +9,8 @@ import com.jacaranda.partituras.PentagramaException;
 
 public class Main {
 
-	public static void main(String[] args) throws PartituraException, NotaException, PentagramaException, CloneNotSupportedException {
-		ConAutor nuvole = new ConAutor("Nuvole Bianche", "Ludovico Einaudi");
-//		nuvole.addPentagrama(5); exception
-		nuvole.addPentagrama(4);
-		Pentagrama p1 = nuvole.getPentagrama(1);
-//		p1.addNota("gi", "blanca", false); exception
-//		p1.addNota("do", "gris", false); exception
-		p1.addNota("do", "blanca", false);
-		p1.addNota("re", "blanca", false);
-//		p1.addNota("do", "blanca", false); exception
-		nuvole.addPentagrama(4);
-		Pentagrama p2 = nuvole.getPentagrama(2);
-		p2.addNota("do", "blanca", true);
-		p2.addNota("si", "blanca", true);
-		System.out.println(nuvole.mostrarMusica());
-		nuvole.modificarPentagrama(1, p2);
-		System.out.println(nuvole.mostrarMusica());
-		Nota mi = new Nota("mi", "negra", false);
-		nuvole.modificarPentagrama(2,1, mi);
-		System.out.println(nuvole.mostrarMusica());
+	public static void main(String[] args) {
 		
-		//BBDD TABLA: NOTA
 		                                                                                                       
 	}
 
---
+++ 3Ev/Colecciones - BBDD/src/com/jacaranda/principal/Pruebas.java
@@ -0,0 +1,50 @@
+package com.jacaranda.principal;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import com.jacaranda.partituras.ConAutor;
+import com.jacaranda.partituras.Nota;
+import com.jacaranda.partituras.NotaException;
+import com.jacaranda.partituras.PartituraException;
+import com.jacaranda.partituras.Pentagrama;
+import com.jacaranda.partituras.PentagramaException;
+
+public class Pruebas {
+	
+	public static Connection c; //BBDD TABLA: NOTA
+
+	public static void main(String[] args) throws NotaException, PentagramaException, PartituraException {
+		
+		try {
+			c = DriverManager.getConnection("jdbc:oracle:thin:@//localhost:1521/ORCLCDB.localdomain", "PARTITURAS", "PARTITURAS");
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		
+		ConAutor nuvole = new ConAutor("Nuvole Bianche", "Ludovico Einaudi");
+//		nuvole.addPentagrama(5); exception
+		nuvole.addPentagrama(4);
+		Pentagrama p1 = nuvole.getPentagrama(1);
+//		p1.addNota("gi", "blanca", false); exception
+//		p1.addNota("do", "gris", false); exception
+		p1.addNota("do", "blanca", false);
+		p1.addNota("re", "blanca", false);
+//		p1.addNota("do", "blanca", false); exception
+		nuvole.addPentagrama(4);
+		Pentagrama p2 = nuvole.getPentagrama(2);
+		p2.addNota("do", "blanca", true);
+		p2.addNota("si", "blanca", true);
+		System.out.println(nuvole.mostrarMusica());
+		nuvole.modificarPentagrama(1, p2);
+		System.out.println(nuvole.mostrarMusica());
+		Nota mi = new Nota("mi", "negra", false);
+		nuvole.modificarPentagrama(2,1, mi);
+		System.out.println(nuvole.mostrarMusica());
+		
+		
+
+	}
+
+}
